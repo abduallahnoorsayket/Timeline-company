601e8d506c13a5878112b9758c8d1511f286d7f2
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2023-11-07 23:24:50 +0600

    add post modal added

--- src/components/TilimeList.vue
+++ src/components/TilimeList.vue
@@ -7,6 +7,19 @@
     <div class="timeline">
       <div class="container">
         <div class="row">
+          <div class="col-md-12">
+            <div class="float-right">
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                @click="openAddPostModal()"
+              >
+                Add post
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="row">
           <div class="col-lg-12">
             <div class="timeline-container">
               <div class="timeline-continue">
@@ -75,14 +88,99 @@
         </div>
       </div>
     </div>
+    <!-- modal office type  -->
+    <teleport to="body" v-if="showModal">
+      <!-- v-if="isEmployeeAttendModalVisible" -->
+      <div id="modal-window" :class="[showModal ? 'modal' : null]">
+        <!-- :class="[isEmployeeAttendModalVisible ? 'modal' : null]" -->
+        <div class="modal-dialog">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="modal-content">
+                <div class="card-header"></div>
+                <div class="card-body">
+                  <!-- <form
+                    @submit.prevent="submitLoginwithOfficeType"
+                    autocomplete="off"
+                  >
+                    <div class="form-group">
+                      <div class="form-row mt-5 mb-5 mr-3 ml-3">
+                        <div class="col-md-6">
+                          <div class="form-check form-check-inline">
+                            <input
+                              class="form-check-input"
+                              type="radio"
+                              name="inlineRadioOptions"
+                              id="inlineRadio1"
+                              v-model="office_type"
+                              value="Home"
+                              @change="submitLoginwithOfficeType()"
+                            />
+                            <label class="form-check-label" for="inlineRadio1"
+                              >Home Office
+                            </label>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-check form-check-inline">
+                            <input
+                              class="form-check-input"
+                              type="radio"
+                              name="inlineRadioOptions"
+                              id="inlineRadio2"
+                              v-model="office_type"
+                              value="Physical"
+                              @change="submitLoginwithOfficeType()"
+                            />
+                            <label class="form-check-label" for="inlineRadio2"
+                              >Physical Office
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form> -->
+                </div>
+                <div class="modal-footer">
+                  <button
+                    class="mt-0 float-right ml-2 btn btn-success"
+                    @click="CloseModal()"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </teleport>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      showModal: false,
+    };
+  },
   computed: {
     all_posts() {
       return this.$store.state.posts;
+    },
+  },
+  methods: {
+    openAddPostModal() {
+      this.showModal = !this.showModal;
+    },
+    CloseModal() {
+      let m = document.querySelector("#modal-window");
+      m.classList.add("hideModal");
+
+      setTimeout(() => {
+        this.showModal = !this.showModal;
+      }, 500);
     },
   },
 };
